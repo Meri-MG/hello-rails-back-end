6334499220f3c4873d7322770fb7b5c4d03c6ea3
Meri <mgogi13@freeuni.edu.ge>
2022-04-28 15:02:46 +0400

    feat: set api gems and controller

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,17 @@ module HelloRailsBackEnd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:post]
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
